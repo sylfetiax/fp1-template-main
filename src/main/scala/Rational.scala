308a917aa1ffb965904bc5ebe763e092c6bccaed
sylfetiax <chernmakc@gmail.com>
2022-05-26 23:45:55 +0300

    commit 3

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -1,41 +1,46 @@
 package com.tkroman.kpi.y2022.l1
 
+import com.tkroman.kpi.y2022.l1
+
 import scala.annotation.targetName
 
-case class Rational(var nom: Int, var denom: Int){
-  assert(denom!=0)
-  private val g = gcd (nom, denom)
-  denom = denom / g
-  nom = nom / g
-  if (denom<0) {
-    denom = -1 * denom
-    nom = -1 * nom
-  }
-}
+case class Rational(private val nom: Int, private val denom: Int) {
+  def ToString: String = nom + "/" + denom
 
-def ToString(r: Rational): String = {
-  r.nom + "/" + r.denom
+  def add(that: Rational) =
+    Rational(
+      this.nom * that.denom + that.nom * this.denom,
+      this.denom * that.denom
+    )
+
+  def mul(that: Rational): Rational =
+    Rational(
+      this.nom * that.nom,
+      this.denom * that.denom
+    )
+
+  def normalize: Rational =
+    Rational(
+      nom / gcd(nom, denom),
+      denom / gcd(nom, denom)
+    )
 }
 
 def gcd(a: Int, b: Int): Int = {
-  if(b == 0) a else gcd(b, a%b)
+  if (b == 0) a else gcd(b, a % b)
+}
+
+object Rational {
+  def apply(nom: Int, denom: Int): Rational = {
+    assert(denom != 0)
+    val g = gcd(nom, denom)
+    if ((denom / g) < 0) {
+      new Rational(-1 * nom / g, -1 * denom / g)
+    } else
+      new Rational(nom / g, denom / g)
   }
-
-def add(l: Rational, r: Rational): Rational =
-    Rational(l.nom * r.denom + r.nom * l.denom, l.denom * r.denom)
-
-
-def mul(l: Rational, r: Rational): Rational =
-  Rational(l.nom * r.nom, l.denom * r.denom)
-
-
-def normalize(r: Rational): Rational = {
-  Rational(r.nom / gcd(r.nom, r.denom), r.denom / gcd(r.nom, r.denom))
 }
 
 @main def run() =
-  println(Rational(1, -2))
-
-
-
+  println(Rational(10, -2))
 
